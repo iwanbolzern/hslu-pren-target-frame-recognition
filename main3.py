66e50a218b1c30395a324579454c83b9f12431c7
Iwan Bolzern <iwan.bolzern@bluewin.ch>
2017-09-28 13:24:51 +0200

    optimistic main4 added

--- main3.py
+++ main3.py
@@ -4,19 +4,31 @@ import argparse
 import cv2
 
 # load the image
-image = cv2.imread('landing_field_2.jpg')
-imgray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-ret,thresh = cv2.threshold(imgray,127,255,0)
+# image = cv2.imread('landing_field_2.jpg')
 
-# find the contours in the mask
-(im2, cnts, _) = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-print("I found %d black shapes" % (len(cnts)))
-#cv2.imshow("Mask", shapeMask)
 
-# loop over the contours
-for c in cnts:
-    # draw the contour and show it
-    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
-    cv2.imshow("Image", image)
+cap = cv2.VideoCapture(0)  # Webcam Capture
 
-cv2.waitKey(0)
+while True:
+    ret, roi_image = cap.read()
+    imgray = cv2.cvtColor(roi_image, cv2.COLOR_BGR2GRAY)
+    bil_filter = cv2.bilateralFilter(imgray, 11, 17, 17)
+    ret, thresh = cv2.threshold(imgray, 240, 255, cv2.THRESH_BINARY)
+
+    cv2.imshow("Image", thresh)
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # press CTRL and Q to stop the program from running
+        break
+
+    # find the contours in the mask
+    (im2, cnts, _) = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    print("I found %d black shapes" % (len(cnts)))
+    #cv2.imshow("Mask", shapeMask)
+
+    # loop over the contours
+    for c in cnts:
+        # draw the contour and show it
+        cv2.drawContours(roi_image, [c], -1, (0, 255, 0), 2)
+        #cv2.imshow("Image", roi_image)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):       #press CTRL and Q to stop the program from running
+            break

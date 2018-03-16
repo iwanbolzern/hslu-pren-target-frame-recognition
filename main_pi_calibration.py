9bcb473cac270d72466be9b400b9f07fa5fc8c26
Iwan Bolzern <iwan.bolzern@bluewin.ch>
2018-03-16 09:21:52 +0100

    flush added

--- main_pi_calibration.py
+++ main_pi_calibration.py
@@ -1,9 +1,14 @@
+import sys
+
+from datetime import datetime
+
 from target_recognition_pi import TargetRecognition
 
 if __name__ == '__main__':
 
     def offest_received(x, z):
-        print('Target detected with centroid: {}, {}'.format(x, z))
+        print('{}: Target detected with centroid: {}, {}'.format(datetime.now(), x, z))
+        sys.stdout.flush()
 
     target_recognition = TargetRecognition()
     target_recognition.register_callback(offest_received)
